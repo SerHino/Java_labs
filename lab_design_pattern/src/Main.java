581d93efaa13d09f53dac786240b2337ef38cdfc
SerHino <72143759+SerHino@users.noreply.github.com>
2023-11-09 12:26:35 +0200

    Add files via upload

---
+++ lab_design_pattern/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Bookshelf bookshelf = Bookshelf.getInstance();
+        BookGenerator bookGenerator = new BookGenerator();
+
+        Book fantasyBook1 = bookGenerator.createBook("fantasy");
+        Book comicsBook1 = bookGenerator.createBook("comics");
+
+        bookshelf.addBook(fantasyBook1);
+        bookshelf.addBook(comicsBook1);
+
+        Book fantasyBook2 = bookGenerator.createBook("fantasy");
+        bookshelf.addBook(fantasyBook2);
+
+        System.out.println("Books on the bookshelf:");
+        bookshelf.displayBooks();
+        //bookshelf.removeBook(fantasyBook2);
+        //bookshelf.displayBooks();
+    }
+}
